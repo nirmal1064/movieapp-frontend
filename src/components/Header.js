c123275da1a5be93af04ead5b87799018f1a86a9
Nirmal Kumar <nirmal1064@gmail.com>
2021-09-12 21:08:55 +0530

    Conditional rendering of headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const user = useSelector((state) => state.user);
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
@@ -9,9 +11,10 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/register">Register</Nav.Link>
-            <Nav.Link href="/watched">Watched</Nav.Link>
+            {!user.auth && <Nav.Link href="/login">Login</Nav.Link>}
+            {!user.auth && <Nav.Link href="/register">Register</Nav.Link>}
+            {user.auth && <Nav.Link href="/watched">Watched</Nav.Link>}
+            {user.auth && <Nav.Link href="/watchlist">Watchlist</Nav.Link>}
           </Nav>
         </Navbar.Collapse>
       </Container>
