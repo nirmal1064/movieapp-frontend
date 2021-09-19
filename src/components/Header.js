6a68239fc636b7dbe994feab84aa44e2c8b6454a
Nirmal Kumar <nirmal1064@gmail.com>
2021-09-19 16:26:08 +0530

    modified route link to brand

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,9 @@ const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Movie App</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          Movie App
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
