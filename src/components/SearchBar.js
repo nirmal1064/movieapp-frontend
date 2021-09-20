8c889e2ca99d37b0554e1fbdca84d0c2e6574f4a
Nirmal Kumar <nirmal1064@gmail.com>
2021-09-20 22:43:50 +0530

    modified search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { Form, InputGroup } from "react-bootstrap";
-import { Search } from "react-bootstrap-icons";
 import { useDispatch, useSelector } from "react-redux";
 import { searchForMovies } from "../redux/actions/movieActions";
 
@@ -15,22 +13,27 @@ const SearchBar = () => {
     }
   };
 
+  const checkEnterKey = (e) => {
+    if (e.key === "Enter") {
+      submitSearch();
+    }
+  };
+
   return (
-    <div className="searchbar">
-      <InputGroup className="mb-3">
-        <Form.Control
-          aria-describedby="search-icon"
-          type="text"
-          value={searchValue}
-          placeholder="Search Movie or Series"
-          onChange={(e) => setSearchValue(e.target.value)}
-        />
-        <Search
-          style={{ fontSize: "2rem", color: "blue" }}
-          id="search-icon"
-          onClick={() => submitSearch()}
-        />
-      </InputGroup>
+    <div className="searchbar input-group">
+      <input
+        className="form-control border-end-0 border rounded-pill"
+        type="text"
+        value={searchValue}
+        id="movie-search-input"
+        onChange={(e) => setSearchValue(e.target.value)}
+        onKeyPress={checkEnterKey}
+      />
+      <span className="input-group-append">
+        <button className="btn btn-outline-secondary bg-white border-start-0 border rounded-pill ms-n3">
+          <i className="fas fa-search" onClick={() => submitSearch()}></i>
+        </button>
+      </span>
     </div>
   );
 };
