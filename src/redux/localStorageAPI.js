0a6566a68c3ab0c93382d9e281ca28733672bd34
Nirmal Kumar <nirmal1064@gmail.com>
2021-09-25 02:01:41 +0530

    removed unused functions

--- src/redux/localStorageAPI.js
+++ src/redux/localStorageAPI.js
@@ -1,19 +1,3 @@
-const initialState = {};
-
-export const loadFromStorage = () => {
-  return localStorage.getItem("user")
-    ? JSON.parse(localStorage.getItem("user"))
-    : initialState;
-};
-
-export const saveToStorage = (state) => {
-  try {
-    localStorage.setItem("user", JSON.stringify(state));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 export const getToken = () => {
   return localStorage.getItem("user")
     ? JSON.parse(localStorage.getItem("user")).token
