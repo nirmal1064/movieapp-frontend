595e4980817d24a4562a6c9ba5b960b00869d6b5
Nirmal Kumar <nirmal1064@gmail.com>
2021-09-25 13:42:53 +0530

    disabled devtools in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,20 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
 const middleWare = [thunk];
 
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    process.env.NODE_ENV === "development" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middleWare))
 );
 
 export default store;
