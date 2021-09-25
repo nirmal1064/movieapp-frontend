e4af1241d0d6f8b1008d7b1f103527c80a682272
Nirmal Kumar <nirmal1064@gmail.com>
2021-09-25 13:47:07 +0530

    modified register page

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,50 +1,55 @@
 import { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Alert, Button, Card, Form } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  // const [showAlert, setShowAlert] = useState(false);
+  const user = useSelector((state) => state.user);
   const submitForm = (e) => {
     e.preventDefault();
   };
 
   return (
-    <Container className="d-flex justify-content-center">
-      <Form onSubmit={submitForm}>
-        <Form.Group className="mb-3" controlId="formUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            value={username}
-            placeholder="Enter your username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formConfirmPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="text"
-            value={confirmPassword}
-            placeholder="Confirm Password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="success" size="sm" type="submit">
-          Register
-        </Button>
-      </Form>
-    </Container>
+    <Card className="register-card">
+      <Card.Body>
+        <h2 className="text-center mb-4">Register</h2>
+        {user.errMsg && <Alert variant="danger">{user.errMsg}</Alert>}
+        <Form onSubmit={submitForm}>
+          <Form.Group className="mb-3" controlId="formUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              value={username}
+              placeholder="Enter your username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formConfirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="text"
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="success" className="w-100" type="submit">
+            Register
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
   );
 };
 
